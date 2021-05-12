07a879e8176a7fcc2ff9e60494753bb7eeb59f40
RyoichiNakai <elmobasketball0913@me.com>
2021-05-12 16:29:27 +0900

    #2 add plugin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import 'prismjs/themes/prism.css'
+import Vuetify from "vuetify"
+import "./assets/style/index.scss"
 
-export default function (Vue, { router, head, isClient }) {
+import 'prismjs/themes/prism.css'
+import DefaultLayout from '~/layouts/Default.vue'
+
+export default function (Vue, { router, appOptions, head, isClient }) {
   // Set default layout as a global component
+  // Googlr Fontの追加
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Roboto'
+  })
+
+  const opts = {
+    icons: {
+      iconfont: "mdi",
+    },
+  };
+
+  Vue.use(Vuetify);
+  appOptions.vuetify = new Vuetify(opts);
+
   Vue.component('Layout', DefaultLayout)
 }
