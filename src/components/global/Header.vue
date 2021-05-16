99beb2fdcdeea0b1185216a3af13aa7aacef377e
RyoichiNakai <elmobasketball0913@me.com>
2021-05-16 18:39:36 +0900

    refactor header

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -6,47 +6,35 @@
     hide-on-scroll
     scroll-target
   >
-
     <v-toolbar-title>
       {{ siteName }}
     </v-toolbar-title>
-
     <v-spacer />
-
     <v-btn icon>
       <v-icon>fab fa-facebook-square</v-icon>
     </v-btn>
-
     <v-btn icon>
       <v-icon>fab fa-linkedin</v-icon>
     </v-btn>
-
     <v-btn icon>
       <v-icon>fab fa-twitter-square</v-icon>
     </v-btn>
-
     <v-btn icon>
       <v-icon>fab fa-github-square</v-icon>
     </v-btn>
-
     <template v-slot:extension>
       <v-tabs>
-
         <v-tab to="/">
           Home
         </v-tab>
-
         <v-tab to="/about">
           About
         </v-tab>
-
         <v-tab to="/articles/">
           Article
         </v-tab>
-
       </v-tabs>
     </template>
-
   </v-app-bar>
 </template>
 
